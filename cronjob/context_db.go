a6e7f83f97747d148ef063b67e8e66987a12a850
junyong.park <junyong.park@illuminarean.com>
2021-03-18 18:31:14 +0900

    fixup! fix:  추가 DB 연결 가능 하도록 수정2

--- cronjob/context_db.go
+++ cronjob/context_db.go
@@ -30,7 +30,7 @@ func ContextDB(service string, xormEngine *xorm.Engine, kafkaConfig kafka.Config
 func ContextMDB(service string, xormEngine *xorm.Engine, kafkaConfig kafka.Config) Middleware {
 	ctxdb := ctxdb.New(xormEngine, service, kafkaConfig)
 
-	return func(job, action string, next HandlerFunc) HandlerFunc {
+	return func(next HandlerFunc) HandlerFunc {
 		return func(ctx context.Context) error {
 			session := ctxdb.NewSession(ctx)
 			defer session.Close()
